670f186441604f16a7b6a09ebb62e97e9d31957f
Suraj_eata <eatasuraj@gmail.com>
2025-12-11 17:17:15 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
+
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    // The code below enables dev tools like taking screenshots of your site
-    // while it is being developed on chef.convex.dev.
-    // Feel free to remove this code if you're no longer developing your app with Chef.
+  
     mode === "development"
       ? {
           name: "inject-chef-dev",
@@ -33,7 +31,7 @@ window.addEventListener('message', async (message) => {
           },
         }
       : null,
-    // End of code for taking screenshots on chef.convex.dev.
+    
   ].filter(Boolean),
   resolve: {
     alias: {
